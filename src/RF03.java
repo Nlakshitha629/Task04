a001a7bdefd35b66625baae82e2754fb4afbc613
Nlakshitha629 <N.L.Yakupitiyage@wlv.ac.uk>
2021-02-09 01:13:01 +0530

    Add third RF03 File

---
+++ src/RF03.java
@@ -0,0 +1,13 @@
+
+
+public class RF03 {
+
+	public static void main(String[] args) {
+		
+		Simple h = new Simple();
+		System.out.println("class =" + h.getClass());
+		System.out.println("class name =" + h.getClass().getName());
+
+	}
+
+}
