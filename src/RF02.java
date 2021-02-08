3e4ac7fe202b418b7ba02c374a44e5210b7f37f8
Nlakshitha629 <N.L.Yakupitiyage@wlv.ac.uk>
2021-02-09 01:11:43 +0530

    Add Second RF02 File

---
+++ src/RF02.java
@@ -0,0 +1,19 @@
+
+
+public class RF02 {
+	
+	public static void main(String args[]) {
+		Simple h = new Simple();
+		h.squareAB();
+
+		
+		float Number1 = h.ab;
+		
+
+		System.out.println("Number1= " + Number1);
+	}
+
+
+
+
+}
